ff3eb56f1d9b2abab3b63bab689d56ac8a678ec8
Samuel Evans-Powell <mail@sevanspowell.net>
2019-01-15 22:11:11 +1000

    Improve xmonad config

---
+++ xmonad/lib/MyKeys.hs
@@ -0,0 +1,42 @@
+module MyKeys where
+
+import XMonad
+
+import XMonad.Actions.Navigation2D
+
+import XMonad.Util.CustomKeys
+
+myKeys = customKeys removedKeys addedKeys
+
+removedKeys :: XConfig l -> [(KeyMask, KeySym)]
+removedKeys _ = []
+
+addedKeys :: XConfig l -> [((KeyMask, KeySym), X ())]
+addedKeys conf @ (XConfig { XMonad.modMask = modMask }) = [ 
+              -- Terminal
+              ((modMask, xK_Return), spawn $ XMonad.terminal conf)
+              
+              -- Close window
+            , ((modMask, xK_w), kill)
+  
+              -- Application launcher
+            , ((modMask, xK_d), spawn "dmenu_run")
+
+              -- Launch Emacs
+            , ((modMask .|. shiftMask, xK_e), spawn "emacsclient -c -a emacs")
+
+              -- Launch Internet Browser
+            , ((modMask .|. shiftMask, xK_i), spawn "chromium")
+
+              -- Restart xmonad
+            , ((modMask, xK_r), restart "xmonad" True)
+ 
+              -- Lock screen
+            , ((modMask, xK_o), spawn "xscreensaver-command -lock")
+ 
+              -- Screenshot 1
+            , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+
+              -- Screenshot 2
+            , ((0, xK_Print), spawn "scrot")
+            ]    
---
+++ xmonad/xmonad.hs
@@ -0,0 +1,42 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.InsertPosition
+import XMonad.Util.Run (spawnPipe)
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Prompt
+import System.IO
+import Data.Monoid ((<>))
+import qualified Data.Map as M
+import qualified XMonad.StackSet as W
+
+import XMonad.Layout.BinarySpacePartition
+import XMonad.Layout.Spacing
+import XMonad.Layout.ThreeColumns
+import XMonad.Actions.Navigation2D
+
+import qualified Graphics.X11.Types as XT
+
+import MyKeys
+
+main :: IO ()
+main = do
+  xmproc <- spawnPipe "xmobar"
+  xmonad =<< xmobar myConfig
+
+backgroundColor = "#FFFFFF"
+middleColor     = "#AEAEAE"
+foregroundColor = "#000000"
+
+myConfig = def
+    { borderWidth = 1
+    , manageHook = insertPosition Below Newer <+> manageDocks <+> manageHook def
+    , layoutHook = avoidStruts $ spacingWithEdge 2 emptyBSP ||| spacingWithEdge 2 (ThreeCol 1 (3/100) (-1/3))
+    , handleEventHook = mconcat [ docksEventHook , handleEventHook def ]
+    , logHook = dynamicLogWithPP xmobarPP { ppTitle = xmobarColor "green" "" . shorten 50 }
+    , modMask = mod4Mask
+    , terminal = "urxvt"
+    , focusedBorderColor = foregroundColor
+    , normalBorderColor = middleColor
+    , keys = myKeys 
+    }
