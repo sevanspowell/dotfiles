a364cfb1d4eddfe649063600f6df752d3539e610
Samuel Evans-Powell <mail@sevanspowell.net>
2019-04-22 16:15:33 +1000

    Changes to emacs config

--- emacs/custom.el
+++ emacs/custom.el
@@ -13,7 +13,7 @@
  '(jdee-db-spec-breakpoint-face-colors (cons "#10151C" "#384551"))
  '(package-selected-packages
    (quote
-    (ripgrep diminish projectile base16-theme nix-mode haskell-mode haskell tramp-mode evil-magit magit doom-themes evil-surround which-key evil counsel swiper avy general use-package)))
+    (quelpa-use-package quelpa diminish projectile base16-theme nix-mode haskell-mode haskell tramp-mode evil-magit magit doom-themes evil-surround which-key evil counsel swiper avy general use-package)))
  '(vc-annotate-background "#1D252C")
  '(vc-annotate-color-map
    (list
@@ -42,4 +42,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "CYEL" :family "Iosevka")))))
+ '(default ((t (:inherit nil :stipple nil :background "gray10" :foreground "#bdbdb3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 83 :width normal :foundry "SRC" :family "Hack"))))
+ '(cursor ((t (:background "dark gray")))))
